619c3f5b85359de9d72cddbc79a2c625b96a0353
sminha <sminha8590@naver.com>
2023-12-02 10:57:18 +0900

    feat: RegisterRequestDTO

---
+++ Downloads/dgtour/dgtour/src/main/java/com/dgtour/web/dto/RegisterRequestDTO.java
@@ -0,0 +1,23 @@
+package com.dgtour.web.dto;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RegisterRequestDTO extends PackageDTO {
+    private String category;
+
+    @Override
+    public String toString() {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            return objectMapper.writeValueAsString(this);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            return super.toString();
+        }
+    }
+}
